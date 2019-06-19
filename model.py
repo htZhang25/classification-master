0fde2a24b9041192f1928d9cd949c257d62994eb
Onyanga <43310722+htZhang25@users.noreply.github.com>
2019-06-19 22:18:42 +0800

    Update model.py

--- model.py
+++ model.py
@@ -6,7 +6,6 @@ class Attention(nn.Module):
     def __init__(self, input_dim):
         super(Attention, self).__init__()
         self.weight = nn.Parameter(torch.Tensor(input_dim, input_dim).uniform_(-1., 1.), requires_grad = True)
-
     def forward(self, x):
         return x.mm(self.weight)
 
@@ -19,14 +18,12 @@ class Net1(torch.nn.Module):
         self.conv3 = nn.Conv1d(in_channels=412, out_channels=206, kernel_size=1, padding=0)
         self.conv4 = nn.Conv1d(in_channels=206, out_channels=103, kernel_size=1, padding=0)
         self.conv5 = nn.Conv1d(in_channels=103, out_channels=9, kernel_size=1, padding=0)
-
     def forward(self, x):   
         x = x.view(x.size(0), -1).permute(1, 0)
         x = self.attention(x.float())
         x = x.unsqueeze(2)
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
-  
         x = F.relu(self.conv3(x))
         x = F.relu(self.conv4(x))
         x = torch.sigmoid(self.conv5(x))
